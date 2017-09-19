9b00c981100c0b65ba19e712014317f43ac28fea
CianCusack <cianncusack@gmail.com>
2017-09-19 15:16:42 +0100

    Updated Setup Game and Settings

---
+++ Game/src/Settings.java
@@ -0,0 +1,12 @@
+
+public class Settings {
+	
+	public Settings(int playersInput, String difficultyInput)
+	{
+		players = playersInput;
+		difficulty = difficultyInput;
+	}
+	public int players = 1;
+	
+	public String difficulty;
+}
